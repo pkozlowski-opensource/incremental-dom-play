159b3499d8c8d36037866b17501d58fbff3e638c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2017-08-27 11:06:20 +0200

    chore: add formatting

--- idom_variant.js
+++ idom_variant.js
@@ -18,7 +18,7 @@ function VDomNode(id, nativeEl, type, value, bindings, viewFn) {
 }
 
 function advanceTo(vdom, startIdx, id) {
-    for (var i = startIdx; i <  vdom.length; i++) {
+    for (var i = startIdx; i < vdom.length; i++) {
         if (vdom[i].id === id) {
             return i;
         }
@@ -34,10 +34,10 @@ function deleteNodes(renderer, parentNativeEl, vdom, currentIdx, count) {
     while (len--) {
         elToDelete = deletedEls[len];
 
-        if (elToDelete.type === '#view') {
-          deleteNodes(renderer, parentNativeEl, elToDelete.children, 0, elToDelete.children.length);
+        if (elToDelete.type === "#view") {
+            deleteNodes(renderer, parentNativeEl, elToDelete.children, 0, elToDelete.children.length);
         } else {
-          renderer.removeNode(parentNativeEl, elToDelete.nativeEl);
+            renderer.removeNode(parentNativeEl, elToDelete.nativeEl);
         }
     }
 }
@@ -58,16 +58,16 @@ function registerEventHandlers(renderer, nativeEl, eventHandlers) {
     var len = events.length;
 
     for (var i = 0; i < len; i++) {
-      renderer.addEventListener(nativeEl, events[i], eventHandlers[events[i]]);
+        renderer.addEventListener(nativeEl, events[i], eventHandlers[events[i]]);
     }
 }
 
 function findParentNativeEl(cursor) {
     var parentCursor;
 
-    while (parentCursor = cursor.parentCursor) {
-      var parentEl = parentCursor.vdom[parentCursor.currentIdx -1];
-        if (parentEl.type === '#view') {
+    while ((parentCursor = cursor.parentCursor)) {
+        var parentEl = parentCursor.vdom[parentCursor.currentIdx - 1];
+        if (parentEl.type === "#view") {
             cursor = parentCursor;
         } else {
             return parentEl.nativeEl;
@@ -78,8 +78,8 @@ function findParentNativeEl(cursor) {
 }
 
 function findSibilingNativeEl(cursor) {
-    for (var i = cursor.currentIdx; i<cursor.vdom.length; i++) {
-        if (cursor.vdom[i].type !== '#view') {
+    for (var i = cursor.currentIdx; i < cursor.vdom.length; i++) {
+        if (cursor.vdom[i].type !== "#view") {
             return cursor.vdom[i].nativeEl;
         }
     }
@@ -89,55 +89,50 @@ function findSibilingNativeEl(cursor) {
 
 function appendNativeEl(cursor, nativeEl) {
     if (cursor.creationMode || !cursor.parentCursor) {
-      cursor.renderer.appendChild(cursor.parentNativeEl, nativeEl);
+        cursor.renderer.appendChild(cursor.parentNativeEl, nativeEl);
     } else {
-      var parentEl = cursor.parentCursor.vdom[cursor.parentCursor.currentIdx -1];
-      if (parentEl.type === '#view') {
-        var sibilingEl = findSibilingNativeEl(cursor);
-        if (sibilingEl) {
-            cursor.renderer.insertBefore(cursor.parentNativeEl, sibilingEl, nativeEl);
+        var parentEl = cursor.parentCursor.vdom[cursor.parentCursor.currentIdx - 1];
+        if (parentEl.type === "#view") {
+            var sibilingEl = findSibilingNativeEl(cursor);
+            if (sibilingEl) {
+                cursor.renderer.insertBefore(cursor.parentNativeEl, sibilingEl, nativeEl);
+            } else {
+                cursor.renderer.appendChild(cursor.parentNativeEl, nativeEl);
+            }
         } else {
             cursor.renderer.appendChild(cursor.parentNativeEl, nativeEl);
         }
-      } else {
-        cursor.renderer.appendChild(cursor.parentNativeEl, nativeEl);
-      }
     }
-  
+
     return nativeEl;
 }
 
 function createTextVNode(cursor, elId, value) {
-  var nativeEl = appendNativeEl(cursor, cursor.renderer.createText(value));
-  return new VDomNode(elId, nativeEl, "#text", value, undefined, undefined);
+    var nativeEl = appendNativeEl(cursor, cursor.renderer.createText(value));
+    return new VDomNode(elId, nativeEl, "#text", value, undefined, undefined);
 }
 
 function text(cursor, elId, value) {
     if (cursor.creationMode) {
-
-      cursor.vdom[cursor.vdom.length] = createTextVNode(cursor, elId, value);
-
+        cursor.vdom[cursor.vdom.length] = createTextVNode(cursor, elId, value);
     } else {
+        var elementIdx = advanceTo(cursor.vdom, cursor.currentIdx, elId);
 
-      var elementIdx = advanceTo(cursor.vdom, cursor.currentIdx, elId);
+        if (elementIdx === -1) {
+            //not found at the expected position => create
+            cursor.vdom.splice(cursor.currentIdx, 0, createTextVNode(cursor, elId, value));
+        } else {
+            // found: update
+            var vDomNode = cursor.vdom[elementIdx];
+            if (vDomNode.value !== value) {
+                vDomNode.value = value;
+                cursor.renderer.updateText(vDomNode.nativeEl, value);
+            }
+        }
 
-      if (elementIdx === -1) {
-
-        //not found at the expected position => create
-        cursor.vdom.splice(cursor.currentIdx, 0, createTextVNode(cursor, elId, value));
-
-      } else {
-          // found: update
-          var vDomNode = cursor.vdom[elementIdx];
-          if (vDomNode.value !== value) {
-              vDomNode.value = value;
-              cursor.renderer.updateText(vDomNode.nativeEl, value);
-          }
-      }
-
-      if (elementIdx > cursor.currentIdx) {
-          deleteNodes(cursor.renderer, cursor.parentNativeEl, cursor.vdom, cursor.currentIdx, elementIdx - cursor.currentIdx);
-      }
+        if (elementIdx > cursor.currentIdx) {
+            deleteNodes(cursor.renderer, cursor.parentNativeEl, cursor.vdom, cursor.currentIdx, elementIdx - cursor.currentIdx);
+        }
     }
 
     cursor.currentIdx++;
@@ -146,19 +141,18 @@ function text(cursor, elId, value) {
 }
 
 function createElementVNode(cursor, elId, tagName, attrs, bindings, eventHandlers) {
-  var nativeEl = cursor.renderer.createElement(tagName);
+    var nativeEl = cursor.renderer.createElement(tagName);
 
-  if (attrs != null) {
-     setNativeAttrs(cursor.renderer, nativeEl, attrs);
-  }
-  if (bindings) {
+    if (attrs != null) {
+        setNativeAttrs(cursor.renderer, nativeEl, attrs);
+    }
+    if (bindings) {
         if (bindings.classes) {
-
             var classNames = Object.keys(bindings.classes);
             var len = classNames.length;
             var className, classValue;
 
-            for (var i=0; i<len; i++) {
+            for (var i = 0; i < len; i++) {
                 className = classNames[i];
                 classValue = bindings.classes[className];
                 if (classValue) {
@@ -172,139 +166,133 @@ function createElementVNode(cursor, elId, tagName, attrs, bindings, eventHandler
             var len = names.length;
             var name;
 
-            for (var i=0; i<len; i++) {
+            for (var i = 0; i < len; i++) {
                 name = names[i];
                 cursor.renderer.setStyle(nativeEl, name, bindings.styles[name]);
             }
         }
 
-      if (bindings.props) {
-          var names = Object.keys(bindings.props);
-          var len = names.length;
-          var name;
+        if (bindings.props) {
+            var names = Object.keys(bindings.props);
+            var len = names.length;
+            var name;
 
-          for (var i=0; i<len; i++) {
-              name = names[i];
-              cursor.renderer.setProperty(nativeEl, name, bindings.props[name]);
-          }
-      }
+            for (var i = 0; i < len; i++) {
+                name = names[i];
+                cursor.renderer.setProperty(nativeEl, name, bindings.props[name]);
+            }
+        }
 
-      if (bindings.attrs) {
-          var names = Object.keys(bindings.attrs);
-          var len = names.length;
-          var name, value;
+        if (bindings.attrs) {
+            var names = Object.keys(bindings.attrs);
+            var len = names.length;
+            var name, value;
 
-          for (var i=0; i<len; i++) {
-              name = names[i];
-              value = bindings.attrs[name];
-              if (value != null) {
-                cursor.renderer.setAttribute(nativeEl, name, value);
-              }
-          }
-      }
+            for (var i = 0; i < len; i++) {
+                name = names[i];
+                value = bindings.attrs[name];
+                if (value != null) {
+                    cursor.renderer.setAttribute(nativeEl, name, value);
+                }
+            }
+        }
     }
-  if (eventHandlers != null) {
-      registerEventHandlers(cursor.renderer, nativeEl, eventHandlers);
-  }
-  appendNativeEl(cursor, nativeEl);
+    if (eventHandlers != null) {
+        registerEventHandlers(cursor.renderer, nativeEl, eventHandlers);
+    }
+    appendNativeEl(cursor, nativeEl);
 
-  return new VDomNode(elId, nativeEl, tagName, undefined, bindings, undefined);
+    return new VDomNode(elId, nativeEl, tagName, undefined, bindings, undefined);
 }
 
 function element(cursor, elId, tagName, attrs, bindings, eventHandlers) {
     if (cursor.creationMode) {
-
-      cursor.vdom[cursor.vdom.length] = createElementVNode(cursor, elId, tagName, attrs, bindings, eventHandlers);
-
+        cursor.vdom[cursor.vdom.length] = createElementVNode(cursor, elId, tagName, attrs, bindings, eventHandlers);
     } else {
+        var elementIdx = advanceTo(cursor.vdom, cursor.currentIdx, elId);
 
-      var elementIdx = advanceTo(cursor.vdom, cursor.currentIdx, elId);
+        if (elementIdx === -1) {
+            cursor.vdom.splice(cursor.currentIdx, 0, createElementVNode(cursor, elId, tagName, attrs, bindings, eventHandlers));
+        } else {
+            // found: update
+            var vDomNode = cursor.vdom[elementIdx];
+            if (bindings) {
+                if (bindings.classes) {
+                    var classNames = Object.keys(bindings.classes);
+                    var len = classNames.length;
+                    var className, classValue;
 
-      if (elementIdx === -1) {
-          cursor.vdom.splice(cursor.currentIdx, 0, createElementVNode(cursor, elId, tagName, attrs, bindings, eventHandlers));
+                    for (var i = 0; i < len; i++) {
+                        className = classNames[i];
 
-      } else {
-        // found: update
-        var vDomNode = cursor.vdom[elementIdx];
-        if (bindings) {
-            if (bindings.classes) {
-
-                var classNames = Object.keys(bindings.classes);
-                var len = classNames.length;
-                var className, classValue;
-
-                for (var i=0; i<len; i++) {
-                    className = classNames[i];
-
-                    classValue = bindings.classes[className];
-                    if (vDomNode.bindings.classes[className] !== classValue) {
-                        vDomNode.bindings.classes[className] = classValue;
-                        if (classValue) {
-                            cursor.renderer.addClass(vDomNode.nativeEl, className);
-                        } else {
-                            cursor.renderer.removeClass(vDomNode.nativeEl, className);
+                        classValue = bindings.classes[className];
+                        if (vDomNode.bindings.classes[className] !== classValue) {
+                            vDomNode.bindings.classes[className] = classValue;
+                            if (classValue) {
+                                cursor.renderer.addClass(vDomNode.nativeEl, className);
+                            } else {
+                                cursor.renderer.removeClass(vDomNode.nativeEl, className);
+                            }
                         }
                     }
                 }
-            }
 
-            if (bindings.styles) {
-                var names = Object.keys(bindings.styles);
-                var len = names.length;
-                var name, value;
+                if (bindings.styles) {
+                    var names = Object.keys(bindings.styles);
+                    var len = names.length;
+                    var name, value;
 
-                for (var i=0; i<len; i++) {
-                    name = names[i];
+                    for (var i = 0; i < len; i++) {
+                        name = names[i];
 
-                    value = bindings.styles[name];
-                    if (vDomNode.bindings.styles[name] !== value) {
-                        vDomNode.bindings.styles[name] = value;
-                        cursor.renderer.setStyle(vDomNode.nativeEl, name, value);
+                        value = bindings.styles[name];
+                        if (vDomNode.bindings.styles[name] !== value) {
+                            vDomNode.bindings.styles[name] = value;
+                            cursor.renderer.setStyle(vDomNode.nativeEl, name, value);
+                        }
                     }
                 }
-            }
 
-            if (bindings.props) {
-                var names = Object.keys(bindings.props);
-                var len = names.length;
-                var name, value;
+                if (bindings.props) {
+                    var names = Object.keys(bindings.props);
+                    var len = names.length;
+                    var name, value;
 
-                for (var i=0; i<len; i++) {
-                    name = names[i];
+                    for (var i = 0; i < len; i++) {
+                        name = names[i];
 
-                    value = bindings.props[name];
-                    if (vDomNode.bindings.props[name] !== value) {
-                        vDomNode.bindings.props[name] = value;
-                        cursor.renderer.setProperty(vDomNode.nativeEl, name, value);
+                        value = bindings.props[name];
+                        if (vDomNode.bindings.props[name] !== value) {
+                            vDomNode.bindings.props[name] = value;
+                            cursor.renderer.setProperty(vDomNode.nativeEl, name, value);
+                        }
                     }
                 }
-            }
 
-            if (bindings.attrs) {
-                var names = Object.keys(bindings.attrs);
-                var len = names.length;
-                var name, value;
+                if (bindings.attrs) {
+                    var names = Object.keys(bindings.attrs);
+                    var len = names.length;
+                    var name, value;
 
-                for (var i=0; i<len; i++) {
-                    name = names[i];
-                    value = bindings.attrs[name];
-                    if (vDomNode.bindings.attrs[name] != value) {
-                        vDomNode.bindings.attrs[name] != value;
-                        if (value != null) {
-                            cursor.renderer.setAttribute(vDomNode.nativeEl, name, value);
-                        } else {
-                            cursor.renderer.removeAttribute(vDomNode.nativeEl, name);
+                    for (var i = 0; i < len; i++) {
+                        name = names[i];
+                        value = bindings.attrs[name];
+                        if (vDomNode.bindings.attrs[name] != value) {
+                            vDomNode.bindings.attrs[name] != value;
+                            if (value != null) {
+                                cursor.renderer.setAttribute(vDomNode.nativeEl, name, value);
+                            } else {
+                                cursor.renderer.removeAttribute(vDomNode.nativeEl, name);
+                            }
                         }
                     }
                 }
             }
         }
-      }
 
-      if (elementIdx > cursor.currentIdx) {
-          deleteNodes(cursor.renderer, cursor.parentNativeEl, cursor.vdom, cursor.currentIdx, elementIdx - cursor.currentIdx);
-      }
-
+        if (elementIdx > cursor.currentIdx) {
+            deleteNodes(cursor.renderer, cursor.parentNativeEl, cursor.vdom, cursor.currentIdx, elementIdx - cursor.currentIdx);
+        }
     }
 
     cursor.currentIdx++;
@@ -312,33 +300,30 @@ function element(cursor, elId, tagName, attrs, bindings, eventHandlers) {
 }
 
 function createViewVDomNode(cursor, elId, viewFn) {
-  return new VDomNode(elId, undefined, "#view", undefined, undefined, viewFn);
+    return new VDomNode(elId, undefined, "#view", undefined, undefined, viewFn);
 }
 
 function view(cursor, elId, viewFn, data) {
     if (cursor.creationMode) {
-
-      cursor.vdom[cursor.vdom.length] = createViewVDomNode(cursor, elId, viewFn);
-
+        cursor.vdom[cursor.vdom.length] = createViewVDomNode(cursor, elId, viewFn);
     } else {
+        var elementIdx = advanceTo(cursor.vdom, cursor.currentIdx, elId);
 
-      var elementIdx = advanceTo(cursor.vdom, cursor.currentIdx, elId);
-
-      if (elementIdx === -1) {
-        //not found at the expected position => create
-        cursor.vdom.splice(cursor.currentIdx, 0, createViewVDomNode(cursor, elId, viewFn));
-      } else {
-        // check if the viewFn changed and if so, cleanup the existing view and update VDOM
-        var vdomNode = cursor.vdom[elementIdx];
-        if (vdomNode.viewFn !== viewFn) {
-            deleteNodes(cursor.renderer, cursor.parentNativeEl, vdomNode.children, 0, vdomNode.children.length);
-            cursor.vdom[elementIdx].viewFn = viewFn;
+        if (elementIdx === -1) {
+            //not found at the expected position => create
+            cursor.vdom.splice(cursor.currentIdx, 0, createViewVDomNode(cursor, elId, viewFn));
+        } else {
+            // check if the viewFn changed and if so, cleanup the existing view and update VDOM
+            var vdomNode = cursor.vdom[elementIdx];
+            if (vdomNode.viewFn !== viewFn) {
+                deleteNodes(cursor.renderer, cursor.parentNativeEl, vdomNode.children, 0, vdomNode.children.length);
+                cursor.vdom[elementIdx].viewFn = viewFn;
+            }
         }
-      }
 
-      if (elementIdx > cursor.currentIdx) {
-          deleteNodes(cursor.renderer, cursor.parentNativeEl, cursor.vdom, cursor.currentIdx, elementIdx - cursor.currentIdx);
-      }
+        if (elementIdx > cursor.currentIdx) {
+            deleteNodes(cursor.renderer, cursor.parentNativeEl, cursor.vdom, cursor.currentIdx, elementIdx - cursor.currentIdx);
+        }
     }
 
     cursor.currentIdx++;
@@ -351,16 +336,13 @@ function component(cursor, elId, componentClass, inputs) {
     var cmptInstance;
 
     if (cursor.creationMode) {
-
-        cursor.vdom[cursor.vdom.length] = createViewVDomNode(cursor, elId, cmptInstance = new componentClass());
-
+        cursor.vdom[cursor.vdom.length] = createViewVDomNode(cursor, elId, (cmptInstance = new componentClass()));
     } else {
-
         var elementIdx = advanceTo(cursor.vdom, cursor.currentIdx, elId);
 
         if (elementIdx === -1) {
             //not found at the expected position => create
-            cursor.vdom.splice(cursor.currentIdx, 0, createViewVDomNode(cursor, elId, cmptInstance = new componentClass()));
+            cursor.vdom.splice(cursor.currentIdx, 0, createViewVDomNode(cursor, elId, (cmptInstance = new componentClass())));
         } else {
             var vdomNode = cursor.vdom[elementIdx];
             cmptInstance = cursor.vdom[elementIdx].viewFn;
